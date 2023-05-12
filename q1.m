41305c06a7141e0a30e9c5073ce3d5f19ba22658
Fazalcode1 <126341873+Fazalcode1@users.noreply.github.com>
2023-05-12 22:43:59 +0530

    Update q1.m

--- q1.m
+++ q1.m
@@ -1,8 +1,4 @@
-question 1
-
-
+%% Question 1 (Linear Algebra)
   
-
-  
-
-  
+  Given a matrix A=[], vector B=[]. A vector v is called a least squares solution of the linear system Ax = B iff ||Av-B||<=||Az-B|| 
+  for all z belongs to Rnx1. So find the value of Av such that v is a least squares solution.
