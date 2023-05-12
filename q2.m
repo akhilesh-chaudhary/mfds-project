04ec2f5df1d7af288d6eb1edbfef294b718ab724
Fazalcode1 <126341873+Fazalcode1@users.noreply.github.com>
2023-05-12 23:42:59 +0530

    Update q2.m

--- q2.m
+++ q2.m
@@ -1,4 +1,4 @@
 %% Question 2
   
-  Consider the LU-decomposotion of a 3x3 matrix A such that U has the eigen values 1,2,3. (Some instrutions for L and U). Then which of
+  Consider the LU-decomposotion of a 3x3 matrix A such that U has the eigen values 1,1,3. (Some instrutions for L and U). Then which of
   the following matrix is not a possibility of A.
